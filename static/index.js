8ffac69fbcf4f2f5adccaccad1331be7ab64274e
名字已消失了 <48387476+mzyxsl@users.noreply.github.com>
2022-01-24 23:10:57 +0800

    Create index.js

--- static/index.js
+++ static/index.js
@@ -341,10 +341,10 @@ function shareText(score) {
         return '倒计时多了' + ((deviation_time / 1000) - 20).toFixed(2) + "s";
     }
     SubmitResults();
-    if (score <= 49) return '试着好好练一下？';
+    if (score <= 49) return '他奶奶的，为什么分数那么低';
     if (score <= 99) return 'TCL';
     if (score <= 149) return 'TQL';
-    if (score <= 199) return '您';
+    if (score <= 199) return '我滴任务完成了，啊哈哈哈哈哈哈';
     return '人？';
 }
 
